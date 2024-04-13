a637dcfdf70478d5f37cadb8217c0d19acf8682b
Stefano Pigozzi <me@steffo.eu>
2024-04-13 23:56:36 +0200

    OH COME ON

--- behaviours/hunter.gd
+++ behaviours/hunter.gd
@@ -12,10 +12,12 @@ var possible_targets: Array = []
 
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
-	if body.find_children("Edible", "Edible", false, false):
-		Log.p(self, "Tracking a new target: %s" % body)
-		possible_targets.push_back(body)
-		tracked.emit(body)
+	var edibles: Array = body.find_children("Edible", "Edible", false, false)
+	for edible in edibles:
+		if edible.tag == tag:
+			Log.p(self, "Tracking a new target: %s" % body)
+			possible_targets.push_back(body)
+			tracked.emit(body)
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	if body in possible_targets:
