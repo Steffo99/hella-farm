8706a6ab1a28f18eebcee894f1862a3a1927f8b8
Stefano Pigozzi <me@steffo.eu>
2024-04-14 00:03:11 +0200

    Add `Log.w` and `Log.e` function to print in yellow and red to the console respectively

--- log.gd
+++ log.gd
@@ -3,8 +3,22 @@ class_name Log
 ## Logging utilities
 
 
-## Print to standard output.
+## Print to standard output in cyan.
 ##
 ## Pass self to the first parameter.
 static func p(obj: Node, message: String) -> void:
-	print("[%s] %s" % [obj, message])
+	print_rich("[color=cyan]{%s} %s[/color]" % [obj, message])
+
+
+## Print to standard output in yellow.
+##
+## Pass self to the first parameter.
+static func w(obj: Node, message: String) -> void:
+	print_rich("[color=yellow]{%s} %s[/color]" % [obj, message])
+
+
+## Print to standard output in red.
+##
+## Pass self to the first parameter.
+static func e(obj: Node, message: String) -> void:
+	print_rich("[color=red]{%s} %s[/color]" % [obj, message])
