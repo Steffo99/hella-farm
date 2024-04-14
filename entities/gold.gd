f1c2c5233b6625bb1de6b0b9b72037f45fc065ce
Stefano Pigozzi <me@steffo.eu>
2024-04-14 04:39:45 +0200

    Complete `Gold` entity

--- entities/gold.gd
+++ entities/gold.gd
@@ -2,5 +2,18 @@ extends Node2D
 class_name Gold
 
 
+@export var value: int = 1
+
+
 func _on_move(movement: Vector2) -> void:
 	position += movement
+
+func _on_collected(tag: StringName) -> void:
+	var game = MainGame.get_ancestor(self)
+	
+	# TODO: Perhaps use a dictionary in game to store multiple currencies?
+	match tag:
+		&"Gold":
+			game.gold_counter.increase(value)
+	
+	queue_free()
