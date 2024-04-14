ed8e09bd60dc0ea92ad44ec57020180dda0f032f
Stefano Pigozzi <me@steffo.eu>
2024-04-14 03:55:57 +0200

    Add a `HoverDetector` acting as `CaptureArea` to `MoveTowardsMouse`

--- behaviours/move_towards_mouse.gd
+++ behaviours/move_towards_mouse.gd
@@ -3,11 +3,17 @@ class_name MoveTowardsMouse
 
 
 signal move(movement: Vector2)
+signal captured
 
 
 @export_range(-5, 5, 1) var speed := 1.0
 
 @onready var viewport: Viewport = get_viewport()
+
+
+enum State { STILL, CAPTURED }
+
+var state: State = State.CAPTURED
 
 
 func get_relative_mouse_position():
@@ -19,7 +25,12 @@ func get_relative_mouse_position():
 
 
 func _physics_process(delta: float) -> void:
-	var direction: Vector2 = position.direction_to(get_relative_mouse_position())
-	var movement: Vector2 = direction * delta * speed
-	Log.p(self, "Moving: %s" % movement)
-	move.emit(movement)
+	match state:
+		State.CAPTURED:
+			var direction: Vector2 = position.direction_to(get_relative_mouse_position())
+			var movement: Vector2 = direction * delta * speed
+			move.emit(movement)
+
+func _on_capture_area_mouse_entered() -> void:
+	state = State.CAPTURED
+	captured.emit()
