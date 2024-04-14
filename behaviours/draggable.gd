330bac8d0ee915ac24384a32e8fbdec070243961
Stefano Pigozzi <me@steffo.eu>
2024-04-14 06:26:14 +0200

    Rename `Pickable` to `Draggable` and add the `drag` and `drop` methods

---
+++ behaviours/draggable.gd
@@ -0,0 +1,19 @@
+extends Node
+class_name Draggable
+
+
+signal dragged
+signal dropped
+
+
+var being_dragged: bool = false
+
+
+func drag():
+	being_dragged = true
+	dragged.emit()
+
+
+func drop():
+	being_dragged = false
+	dropped.emit()
