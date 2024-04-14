6dde00120d63cbfd6be1bd3e3b2b4e948e74004a
Stefano Pigozzi <me@steffo.eu>
2024-04-14 03:49:28 +0200

    Use `HoverDetector` in `SkitterFromMouse`

--- behaviours/skitter_from_mouse.gd
+++ behaviours/skitter_from_mouse.gd
@@ -12,24 +12,22 @@ signal calmed
 @export var directions: Array[Vector2] = []
 
 @onready var calm_timer: Timer = $"CalmTimer"
+@onready var hover_detector: HoverDetector = $"HoverDetector"
 
-
-var mouse_inside: bool = false
 
 enum State { CALM, SCARED }
 
-var state: State = State.CALM:
-	get:
-		return state
-	set(value):
-		if state != value:
-			state = value
-			match state:
-				State.CALM:
-					calmed.emit()
-				State.SCARED:
-					calm_timer.start(scare_secs)
-					scared.emit()
+var state: State = State.CALM
+
+
+func calm():
+	state = State.CALM
+	calmed.emit()
+
+func scare():
+	state = State.SCARED
+	calm_timer.start(scare_secs)
+	scared.emit()
 
 
 func _ready():
@@ -39,22 +37,20 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	match state:
 		State.SCARED:
-			var idx := Random.rng.randi_range(0, len(directions))
-			var direction: Vector2 = directions[idx]
-			var movement: Vector2 = direction * delta * speed
-			move.emit(movement)
-
-func _on_scare_area_mouse_entered() -> void:
-	mouse_inside = true
-
-func _on_scare_area_mouse_exited() -> void:
-	mouse_inside = false
+			if len(directions) > 0:
+				var idx := Random.rng.randi_range(0, len(directions))
+				var direction: Vector2 = directions[idx]
+				var movement: Vector2 = direction * delta * speed
+				move.emit(movement)
 
 func _on_calm_timer_timeout() -> void:
-	if mouse_inside:
-		calm_timer.start(scare_secs)
+	if hover_detector.mouse_inside:
+		scare()
 	else:
-		state = State.CALM
+		calm()
+
+func _on_hover_detector_mouse_entered() -> void:
+	scare()
 
 func _on_move(movement: Vector2) -> void:
 	Log.p(self, "Moving by: %s" % movement)
@@ -64,4 +60,3 @@ func _on_scared() -> void:
 
 func _on_calmed() -> void:
 	Log.p(self, "Calmed down.")
-
