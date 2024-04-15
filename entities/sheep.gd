fb78e3f001f5bd4472ad144f70474c016432cbe2
Stefano Pigozzi <me@steffo.eu>
2024-04-15 22:20:13 +0200

    Add a couple of sheep animations

--- entities/sheep.gd
+++ entities/sheep.gd
@@ -6,7 +6,11 @@ class_name Sheep
 @onready var sprite: SpriteLeftRight = $"Sprite"
 @onready var drag_sound: AudioStreamPlayer2D = $"Draggable/DragSound"
 @onready var drop_sound: AudioStreamPlayer2D = $"Draggable/DropSound"
+@onready var animator: AnimationPlayer = $"Animator"
 
+
+func _ready() -> void:
+	animator.play("RESET")
 
 func _on_move(movement: Vector2) -> void:
 	if not draggable.being_dragged:
@@ -20,6 +24,8 @@ func _on_drag_move(movement: Vector2) -> void:
 
 func _on_draggable_dragged() -> void:
 	drag_sound.play()
+	animator.play("wobble")
 
 func _on_draggable_dropped() -> void:
 	drop_sound.play()
+	animator.play("RESET")
