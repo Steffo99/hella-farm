e2f4ad461e86f13f177e023be8480d9ef95dcdc7
Stefano Pigozzi <me@steffo.eu>
2024-04-16 01:56:46 +0200

    Refactor `HoverDetector` into `CursorDetector`

---
+++ behaviours/cursor_detector.gd
@@ -0,0 +1,21 @@
+@icon("res://behaviours/cursor_detector.svg")
+extends Area2D
+class_name CursorDetector
+
+
+signal cursor_entered(cursor: Cursor)
+signal cursor_exited(cursor: Cursor)
+
+
+var mouse_inside = false
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Cursor:
+		mouse_inside = true
+		cursor_entered.emit(body)
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Cursor:
+		mouse_inside = true
+		cursor_exited.emit(body)
