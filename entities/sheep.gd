bdde57200fe36529d4016547b0d8749d96e57ae0
Stefano Pigozzi <me@steffo.eu>
2024-04-15 05:43:15 +0200

    Update `Sheep` with sprites

--- entities/sheep.gd
+++ entities/sheep.gd
@@ -3,6 +3,7 @@ class_name Sheep
 
 
 @onready var draggable: Draggable = $"Draggable"
+@onready var sprite: SpriteLeftRight = $"Sprite"
 @onready var drag_sound: AudioStreamPlayer2D = $"Draggable/DragSound"
 @onready var drop_sound: AudioStreamPlayer2D = $"Draggable/DropSound"
 
@@ -10,10 +11,12 @@ class_name Sheep
 func _on_move(movement: Vector2) -> void:
 	if not draggable.being_dragged:
 		move_and_collide(movement)
+		sprite.handle_move(movement)
 
 func _on_drag_move(movement: Vector2) -> void:
 	if draggable.being_dragged:
 		move_and_collide(movement)
+		sprite.handle_move(movement)
 
 func _on_draggable_dragged() -> void:
 	drag_sound.play()
