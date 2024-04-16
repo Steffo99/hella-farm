36c174397993a1d8bba6731b8c87606ac19da177
Stefano Pigozzi <me@steffo.eu>
2024-04-16 03:56:46 +0200

    Break `Draggable`

--- behaviours/draggable.gd
+++ behaviours/draggable.gd
@@ -1,21 +1,13 @@
+@icon("res://behaviours/draggable.svg")
 extends Area2D
 class_name Draggable
 
 
-signal move(movement: Vector2)
 signal dragged
 signal dropped
 
 
-@onready var mover: Node2D = $"MatchMousePosition"
-
-var being_dragged: bool:
-	get:
-		return being_dragged
-	set(value):
-		being_dragged = value
-		mover.set_process(being_dragged)
-		mover.set_physics_process(being_dragged)
+var being_dragged: bool = false
 
 
 func drag():
@@ -25,10 +17,3 @@ func drag():
 func drop():
 	being_dragged = false
 	dropped.emit()
-
-
-func _ready():
-	being_dragged = false
-
-func _on_move(movement: Vector2):
-	move.emit(movement)
