17e87b06d09349fefb9cfaa5c04596fa7ba6d67e
Stefano Pigozzi <me@steffo.eu>
2024-04-17 05:01:02 +0200

    Alter `Move` a bit...

--- behaviours/move.gd
+++ behaviours/move.gd
@@ -4,6 +4,19 @@ class_name Move
 
 
 signal move(norm: Vector2)
+signal move_toggled(value: bool)
+signal move_enabled
+signal move_disabled
 
 ## Whether this component should emit [signal move].
-@export var enabled: bool = true
+@export var enabled: bool = true:
+	get:
+		return enabled
+	set(value):
+		if enabled != value:
+			enabled = value
+			move_toggled.emit(value)
+			if value:
+				move_enabled.emit()
+			else:
+				move_disabled.emit()
