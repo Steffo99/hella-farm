c20eb76fb985efcd477de63aacd4e62b5d6ae8f7
Stefano Pigozzi <me@steffo.eu>
2024-04-19 01:46:25 +0200

    Again, remove `Draggable` from `Imp`

--- entities/imp.gd
+++ entities/imp.gd
@@ -5,7 +5,6 @@ class_name Imp
 
 @onready var skull_spawner: Spawner = $"SkullSpawner"
 @onready var sprite: SpriteLeftRight = $"Sprite"
-@onready var draggable: Draggable = $"Draggable"
 
 
 func _on_eat_target_eaten(target: Edible):
@@ -13,13 +12,6 @@ func _on_eat_target_eaten(target: Edible):
 	if Random.rng.randf() < skull_chance:
 		skull_spawner.spawn()
 
-
-func _on_draggable_move(movement:Vector2):
-	if draggable.being_dragged:
-		move_and_collide(movement)
-		sprite.handle_move(movement)
-
 func _on_move(movement:Vector2):
-	if not draggable.being_dragged:
-		move_and_collide(movement)
-		sprite.handle_move(movement)
+	move_and_collide(movement)
+	sprite.handle_move(movement)
