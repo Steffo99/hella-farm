c2dd8a61cd714d48fa2a204dacac9fa9d864b6c1
Stefano Pigozzi <me@steffo.eu>
2024-04-19 01:48:11 +0200

    Fix `TrackerEdible` still looking for `tag` instead of `diet`

--- behaviours/tracker_edible.gd
+++ behaviours/tracker_edible.gd
@@ -10,6 +10,6 @@ class_name EdibleTracker
 func check_diet_then_track(body: Node2D) -> void:
 	var edibles: Array = body.find_children("*", "Edible", false, false)
 	for edible in edibles:
-		if edible.tag in acceptable_diets:
+		if edible.diet in acceptable_diets:
 			track(body)
 			break
