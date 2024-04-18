081a4f905dc407b79494cd749ddcfb231b9c5d58
Stefano Pigozzi <me@steffo.eu>
2024-04-19 01:52:35 +0200

    Remove more unused references in `Imp`

--- entities/imp.gd
+++ entities/imp.gd
@@ -1,16 +1,9 @@
 extends CharacterBody2D
 class_name Imp
 
-@export var skull_chance: float = 0.2
 
-@onready var skull_spawner: Spawner = $"SkullSpawner"
 @onready var sprite: SpriteLeftRight = $"Sprite"
 
-
-func _on_eat_target_eaten(target: Edible):
-	target.queue_free()
-	if Random.rng.randf() < skull_chance:
-		skull_spawner.spawn()
 
 func _on_move(movement:Vector2):
 	move_and_collide(movement)
