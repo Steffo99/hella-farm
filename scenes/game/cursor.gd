3d9233f01fff211fb67668b10c3e1873ee57682f
Stefano Pigozzi <me@steffo.eu>
2024-04-20 02:17:40 +0200

    Add a `get_via_group` method to `Cursor`

--- scenes/game/cursor.gd
+++ scenes/game/cursor.gd
@@ -7,5 +7,12 @@ class_name Cursor
 @onready var game := MainGame.get_via_group(self)
 
 
+static func get_via_group(node: Node) -> MainGame:
+	var result = node.get_tree().get_nodes_in_group("cursor")
+	if result.is_empty():
+		return null
+	return result[0]
+
+
 func _physics_process(_delta: float) -> void:
 	move_and_collide(game.camera.get_global_mouse_position() - global_position)
