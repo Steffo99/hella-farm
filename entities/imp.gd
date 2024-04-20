704c70b355044376b6d9c4b2258de43a761db7e1
Stefano Pigozzi <me@steffo.eu>
2024-04-20 05:00:23 +0200

    Make `Imp` `Draggable` too!

--- entities/imp.gd
+++ entities/imp.gd
@@ -3,6 +3,7 @@ class_name Imp
 
 
 @onready var sprite: SpriteLeftRight = $"Sprite"
+@onready var eater: Eater = $"Eater"
 
 
 func _on_move(movement: Vector2):
@@ -11,3 +12,16 @@ func _on_move(movement: Vector2):
 
 func _on_eater_eaten(edible: Edible) -> void:
 	edible.get_parent().queue_free()
+
+
+func _on_draggable_dragged(_cursor: Cursor) -> void:
+	collision_layer = 16
+	collision_mask = 18
+	z_index = 1
+	eater.collision_mask = 16
+
+func _on_draggable_dropped() -> void:
+	collision_layer = 8
+	collision_mask = 14
+	z_index = 0
+	eater.collision_mask = 8
