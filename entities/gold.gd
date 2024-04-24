93c3e734f195bd9f65b1c9a0ff654a097c718318
Stefano Pigozzi <me@steffo.eu>
2024-04-24 04:35:35 +0200

    Make `Gold` feel more satisfying to collect

--- entities/gold.gd
+++ entities/gold.gd
@@ -2,6 +2,9 @@ extends Node2D
 class_name Gold
 
 
+@export var speed_up_factor := 8.0
+
+
 @onready var game: MainGame = MainGame.get_via_group(self)
 @onready var move_towards: MoveTowards = %"MoveTowards"
 @onready var collect_sound_spawner: Spawner = %"CollectSoundSpawner"
@@ -22,3 +25,7 @@ func collect() -> void:
 
 func _on_move(movement: Vector2) -> void:
 	position += movement
+
+func _physics_process(delta: float) -> void:
+	if move_towards.target != null:
+		move_towards.speed *= pow(speed_up_factor, delta)
