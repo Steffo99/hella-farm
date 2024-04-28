872ce5673f8ce8a5d14d8b6e42893687b8dfd426
Stefano Pigozzi <me@steffo.eu>
2024-04-29 01:29:33 +0200

    Complete new summoning circle customizer

---
+++ behaviours/summoning_circle.gd
@@ -0,0 +1,72 @@
+@icon("res://behaviours/summoning_circle.svg")
+extends Node2D
+class_name SummoningCircle
+
+
+## Emitted when a recipe is matched, before the components are sacrificed.
+signal recipe_matched(m: SummoningRecipe.Match, recipe: SummoningRecipe)
+
+
+## The [SacrificeStone]s part of this summoning circle.
+var stones: Array[SacrificeStone] = []
+
+## The [SummoningRecipe]s performable by this summoning circle.
+var recipes: Array[SummoningRecipe] = []
+
+var _recipes_matched_signals: Array[Callable] = []
+
+
+## Refresh the value of [field stones], and reconnect signals accordingly.
+func refresh_stones() -> void:
+	# Disconnect signals
+	for stone in stones:
+		if stone == null:
+			return
+		if stone.sacrifice_changed.is_connected(_on_sacrifice_changed):
+			stone.sacrifice_changed.disconnect(_on_sacrifice_changed)
+	# Find the new stones
+	stones.assign(
+		find_children("*", "SacrificeStone", true, false)
+	)
+	# Reconnect signals
+	for stone in stones:
+		stone.sacrifice_changed.connect(_on_sacrifice_changed)
+
+## Refresh the value of [field recipes], and reconnect signals accordingly.
+func refresh_recipes() -> void:
+	# Disconnect signals
+	var idx: int = 0
+	for recipe in recipes:
+		var callable = _recipes_matched_signals[idx]
+		if recipe.matched.is_connected(callable):
+			recipe.matched.disconnect(callable)
+		idx += 1
+	_recipes_matched_signals = []
+	# Find the new recipes
+	recipes.assign(
+		find_children("*", "SummoningRecipe", true, false)
+	)
+	# Reconnect signals
+	for recipe in recipes:
+		var callable = _on_recipe_matched.bind(recipe)
+		recipe.matched.connect(callable)
+		_recipes_matched_signals.push_back(callable)
+
+
+func _ready() -> void:
+	refresh_stones()
+	refresh_recipes()
+
+func _on_sacrifice_changed(_entity: Node2D) -> void:
+	for recipe in recipes:
+		var entities: Array[Node2D] = []
+		entities.assign(
+			stones.map(func(stone): return stone.entity)
+		)
+		if recipe.do_match(entities):
+			break
+
+func _on_recipe_matched(m: SummoningRecipe.Match, _recipe: SummoningRecipe) -> void:
+	recipe_matched.emit()
+	for sacrificable in m.sacrificables:
+		sacrificable.sacrifice()
--- behaviours/summoning_recipe.gd
+++ behaviours/summoning_recipe.gd
@@ -3,59 +3,80 @@ extends Node
 class_name SummoningRecipe
 
 
-## A [Node] describing a possible recipe that can be performed with the [SummoningCircle].
+## A [Node] describing a possible ingredients that can be performed with the [SummoningCircle].
 
 
-## Emitted when [method do_match] is called, and the [field recipe] is successfully matched.
-signal matched
+## Emitted when [method do_match] is called, and the [field ingredients] is successfully matched.
+signal matched(match: Match)
 
 
 ## How [Sacrificable]s should be matched.
 @export var mode := Mode.NONE
 
 ## Which [Sacrificable]s should be matched.
-@export var recipe: Array[StringName] = []
+@export var ingredients: Array[StringName] = []
 
 
-func check_match(inputs: Array[StringName]) -> bool:
-	var matching: Array[bool] = recipe.map(func(): return false)
-	# This is awful, but I am too lazy to implement something better
-	# why is there no enumerate Q_Q
-	for input in inputs:
-		var idx = 0
-		for matchable in recipe:
-			if input == matchable and not matching[idx]:
-				matching[idx] = true
+func get_match(inputs: Array[Node2D]) -> Match:
+	var matched_ingredients: Array[Sacrificable] = []
+	# Find the ingredients
+	for ingredient in ingredients:
+		var matched_this = false
+		for input in inputs:
+			if input == null:
 				continue
-			idx += 1
+			for _sacrificable in input.find_children("*", "Sacrificable", true, false):
+				var sacrificable: Sacrificable = _sacrificable as Sacrificable
+				if sacrificable in matched_ingredients:
+					continue
+				if sacrificable.kind == ingredient:
+					matched_ingredients.push_back(sacrificable)
+					matched_this = true
+					break
+			if matched_this:
+				break
+		if not matched_this:
+			matched_ingredients.push_back(null)
 	# Use one of the various modes
-	# why do i have to do this
 	match mode:
 		Mode.NONE:
-			return false
+			return null
 		Mode.ANY:
-			return matching.any(func(value): return value)
+			if matched_ingredients.any(func(value): return value != null):
+				return Match.create(matched_ingredients)
+			else:
+				return null
 		Mode.ALL:
-			return matching.all(func(value): return value)
-		Mode.EXACT:
-			return matching.all(func(value): return value) and len(inputs) == len(recipe)
+			if matched_ingredients.all(func(value): return value != null):
+				return Match.create(matched_ingredients)
+			else:
+				return null
 		_:
 			# Static analysis go home you're drunk
-			return false
+			return null
 
 
-func do_match(inputs: Array[StringName]) -> void:
-	if check_match(inputs):
-		matched.emit()
+func do_match(inputs: Array[Node2D]) -> Match:
+	var m = get_match(inputs)
+	if m != null:
+		matched.emit(m)
+	return m
+
+
+class Match:
+	var sacrificables: Array[Sacrificable]
+
+	static func create(s: Array[Sacrificable]) -> Match:
+		var this = Match.new()
+		this.sacrificables = s
+		return this
 
 
 enum Mode {
-	## Never match the recipe.
+	## Never match the ingredients.
 	NONE = 0,
-	## Match the recipe if a single one of the sacrificables is matched. 
+	## Match the ingredients if a single one of the sacrificables is matched. 
 	ANY = 1,
-	## Match the recipe if all the sacrificables are matched, even if more are present.
+	## Match the ingredients if all the sacrificables are matched, even if more are present.
 	ALL = 2,
-	## Match the recipe if all the sacrificables are matched AND there are no other sacrificables present.
-	EXACT = 3,
 }
