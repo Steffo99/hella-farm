fefcacef4b65399d9d3eece04ac2bdb2b19f7e37
Stefano Pigozzi <me@steffo.eu>
2024-04-28 17:16:36 +0200

    Move `SacrificeKind` to `Sacrificable`

--- behaviours/sacrificable.gd
+++ behaviours/sacrificable.gd
@@ -2,6 +2,7 @@
 extends Node
 class_name Sacrificable
 
+
 ## Emits [signal eaten] when eaten by an [Eater] whose acceptable diets contain this node's [field diet].
 ##
 ## To add multiple possible [Edible] diets to an entity, add multiple [Edible] nodes to it.
@@ -9,8 +10,18 @@ class_name Sacrificable
 signal sacrificed
 
 
-@export var type: Enums.MonsterType = Enums.MonsterType.Sheep
+@export var type: SacrificeKind = SacrificeKind.Sheep
 
 
 func sarcifice():
 	sacrificed.emit()
+
+
+enum SacrificeKind {
+    None = 0, # I hate this but in GDScript enums can't be nulls and there aren't any sum types like in rust so this is all we can do
+    Sheep,
+    Imp,
+    Chupacabra,
+    Watcher,
+    Cthulhu,
+}
