872ff3c6b62ab343c17350af0c7ee114dd47e695
Stefano Pigozzi <me@steffo.eu>
2024-04-28 19:43:10 +0200

    Convert `Sacrificable`'s enum into a `StringName`
    Sigh, Godot displays an array of enums as ints...

--- behaviours/sacrificable.gd
+++ behaviours/sacrificable.gd
@@ -10,18 +10,8 @@ class_name Sacrificable
 signal sacrificed
 
 
-@export var type: SacrificeKind = SacrificeKind.Sheep
+@export var kind: StringName = &""
 
 
 func sarcifice():
 	sacrificed.emit()
-
-
-enum SacrificeKind {
-    None = 0, # I hate this but in GDScript enums can't be nulls and there aren't any sum types like in rust so this is all we can do
-    Sheep,
-    Imp,
-    Chupacabra,
-    Watcher,
-    Cthulhu,
-}
