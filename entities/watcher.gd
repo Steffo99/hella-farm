a508ef549073e9b4512243476f84a64f3d9ad995
Stefano Pigozzi <me@steffo.eu>
2024-04-30 03:06:20 +0200

    idr

--- entities/watcher.gd
+++ entities/watcher.gd
@@ -14,17 +14,9 @@ func _on_move(movement: Vector2):
 	sprite.handle_move(movement)
 
 func _on_dragged(_cursor: Cursor) -> void:
-	collision_layer = 16
-	collision_mask = 18
-	z_index = Enums.ZIndex.EntityAir
-	y_sort_enabled = false
 	animator.play(&"drag_start")
 
 func _on_fallen() -> void:
-	collision_layer = 8
-	collision_mask = 14
-	z_index = Enums.ZIndex.EntityGround
-	y_sort_enabled = true
 	animator.play(&"RESET")
 
 func _on_trapped() -> void:
@@ -32,5 +24,5 @@ func _on_trapped() -> void:
 	y_sort_enabled = false
 
 func _on_freed() -> void:
-	z_index = Enums.ZIndex.EntityGround
+	z_index = Enums.ZIndex.EntityAir
 	y_sort_enabled = true
